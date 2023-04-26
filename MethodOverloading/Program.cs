fe3332ae2704b063108d9d8c3a84ec3d80e4ee7b
msimangoksi@gmail.com <msimangoksi@gmail.com>
2023-04-26 15:58:56 +0200

    Add project files.

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Msimango KSI
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamePets("Mia", "Arial");
+            NamePets("Mango", "Puddy", "Bucket");
+            NamePets();
+        }
+
+        static void NamePets(string pet1, string pet2)
+        {
+            Console.WriteLine($"Your pets {pet1} and {pet2} will be joining your voyage across space!");
+        }
+
+        static void NamePets(string pet1, string pet2, string pet3)
+        {
+            Console.WriteLine($"Your pets {pet1}, {pet2}, and {pet3} will be joining your voyage across space!");
+        }
+
+        static void NamePets()
+        {
+            Console.WriteLine("Aw, you have no spacefaring pets :(");
+        }
+    }
+}
